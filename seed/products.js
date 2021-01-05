c28fb1ce0e7b33024735f4774dc2e68bc6b140b5
Liz Yrineo <lizyrineo@yahoo.com>
2021-01-05 09:42:55 -0700

    app.js complete

---
+++ seed/products.js
@@ -0,0 +1,12 @@
+const db = require('../db/connection')
+const Product = require('../models/product')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+  const products = 
+    [
+      {
+      
+    }
+  ]
