c6ed3e5de4edf307f18ddea4b7048bcbc98d017d
Evyn Newton <newton.evyn@gmail.com>
2021-01-06 16:44:28 -0500

    carousel +/- working

--- client/src/images/HomepageCarousel/index.js
+++ client/src/images/HomepageCarousel/index.js
@@ -1,9 +1,8 @@
-export const First = require('./photoOne.jpeg');
-export const Second = require('./photoTwo.jpeg');
-export const Third = require('./photoThree.jpeg');
-export const Fourth = require('./photoFour.jpeg');
-export const Fifth = require('./photoFive.jpeg');
-export const Sixth = require('./photoSix.jpeg');
+export const First = require("./photoOne.jpeg");
+export const Second = require("./photoTwo.jpeg");
+export const Third = require("./photoThree.jpeg");
+export const Fourth = require("./photoFour.jpeg");
+export const Fifth = require("./photoFive.jpeg");
+export const Sixth = require("./photoSix.jpeg");
 
-
-
+export const images = [First, Second, Third, Fourth, Fifth, Sixth];
--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -2,11 +2,12 @@ import Layout from "../../components/shared/Layout/Layout";
 import Carousel from "../../components/Carousel/Carousel";
 import "./Landing.css";
 import { Link } from "react-router-dom";
+import { images } from "../../images/HomepageCarousel/index";
 
 export default function Landing(props) {
   return (
     <Layout user={props.user}>
-      <Carousel />
+      <Carousel images={images} />
       <Link className="day-trip-link" to={`/listing-page/Day-Trip`}>
         <div className="shop-day-trip">Shop Day Trip</div>
       </Link>
