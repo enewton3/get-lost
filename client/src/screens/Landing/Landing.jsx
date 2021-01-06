ad2a4515d91e9d9e6de3b4fe680cb0fd1a4da2d7
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2021-01-06 07:09:02 -0800

    updated Landing component to link to filtered Listing

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,11 +1,21 @@
 import Layout from "../../components/shared/Layout/Layout";
 import Carousel from "../../components/Carousel/Carousel";
-import './Landing.css'
+import "./Landing.css";
+import { Link } from "react-router-dom";
 
 export default function Landing(props) {
   return (
-      <Layout user={props.user}>
-        <Carousel />
-      </Layout>
+    <Layout user={props.user}>
+      <Carousel />
+      <Link to={`/Listing/Day-Trip`}>
+        <div className="shop-day-trip">Shop Day Trip</div>
+      </Link>
+      <Link to={`/Listing/Expedition`}>
+        <div className="shop-expedition">Shop Day Trip</div>
+      </Link>
+      <Link to={`/Listing/all`}>
+        <div className="shop-all">Shop Day Trip</div>
+      </Link>
+    </Layout>
   );
 }
