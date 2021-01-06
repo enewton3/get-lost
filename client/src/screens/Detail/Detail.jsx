bf5bf7ce28e8f02860e1b121bc3b2c3ac8073d1f
Evyn Newton <newton.evyn@gmail.com>
2021-01-06 10:32:33 -0500

    clearing errors

--- client/src/screens/Detail/Detail.jsx
+++ client/src/screens/Detail/Detail.jsx
@@ -1,41 +1,48 @@
-import React from "react";
-import '/Detail.css';
-import Layout from '../../components/shared/Layout/Layout';
-import { getProduct, deleteProduct } from '../../services/products'
-import {useParams, Link} from 'react-router-dom'
+import "./Detail.css";
+import { useState, useEffect } from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import { getProduct, deleteProduct } from "../../services/products";
+import { useParams, Link } from "react-router-dom";
 
 const Detail = (props) => {
+  const [product, setProduct] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
 
-  const [product, setProduct] = useState(null)
-  const [isLoaded, setLoaded] = useState(false)
-  const { id } = useParams()
-  
   useEffect(() => {
     const fetchProduct = async () => {
-      const product = await getProduct(id)
-      setProduct(product)
-      setLoaded(true)
-    }
-    fetchProduct()
-  }, [id])
+      const product = await getProduct(id);
+      setProduct(product);
+      setLoaded(true);
+    };
+    fetchProduct();
+  }, [id]);
 
   if (!isLoaded) {
-    return <h1>The journey is worth the wait!</h1>
+    return <h1>The journey is worth the wait!</h1>;
   }
   return (
     <Layout user={props.user}>
-      <div className='product-detail'>
-        <div className='name'>{product.name}</div>
-        <div className='price'>{`${product.price}`}</div>
-        <div className='description'>{product.description}</div>
-        <img className='detail-image' src={product.imgURL} alt={product.name} />
-        <button className='edit-button' onClick={() => editProduct(product._id)}><FontAwesomeIcon icon={fa-trash-alt} /></button>
-        <button className='delete-button' onClick={() => deleteProduct(product.id)}><FontAwesomeIcon icon={fa-pencil-alt} /></button>
+      <div className="product-detail">
+        <div className="name">{product.name}</div>
+        <div className="price">{`${product.price}`}</div>
+        <div className="description">{product.description}</div>
+        <img className="detail-image" src={product.imgURL} alt={product.name} />
+        <button
+          className="edit-button"
+          onClick={() => editProduct(product._id)}
+        >
+          <FontAwesomeIcon icon={fa - trash - alt} />
+        </button>
+        <button
+          className="delete-button"
+          onClick={() => deleteProduct(product.id)}
+        >
+          <FontAwesomeIcon icon={fa - pencil - alt} />
+        </button>
       </div>
-  </Layout>
-)
+    </Layout>
+  );
+};
 
-}
-
-export default Detail
-
+export default Detail;
