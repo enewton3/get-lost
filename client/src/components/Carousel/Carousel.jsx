5ee4d2f0f9e6b5de159235258f598e8fc6c706f2
Liz Yrineo <lizyrineo@yahoo.com>
2021-01-06 13:44:35 -0700

    started carousel

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { useState } from 'react-router-dom';
+import ArrowLeft from './ArrowLeft';
+import ArrowRight from './ArrowRight';
+import Slide from './Slide';
+import Indicator from './Indicator';
+import { First, Second, Third, Fourth, Fifth, Sixth } from '../../images/HomepageCarousel';
+
+const images = [
+  First,
+  Second,
+  Third,
+  Fourth,
+  Fifth,
+  Sixth
+] 
 
 export default function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index)
+    
+  }
   return <div>Carousel</div>;
 }
