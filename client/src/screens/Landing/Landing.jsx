90b626794bac7289b1133b451e34fdbcb2b8dc82
Mitchel Noble <Mitch@Mitchels-MacBook-Pro.local>
2021-01-06 06:44:31 -0800

    added skeleton function for Carousel.jsx and added tag into Landing.jsx

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import Layout from "../../components/shared/Layout/Layout";
+import Carousel from "../../components/Carousel/Carousel";
+import './Landing.css'
 
 export default function Landing(props) {
   return (
-    <div>
-      <Layout user={props.user}></Layout>
-    </div>
+      <Layout user={props.user}>
+        <Carousel />
+      </Layout>
   );
 }
