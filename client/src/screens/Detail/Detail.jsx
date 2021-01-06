c8510e46d0dc013bdf809dfd03c2f241ee9de045
Evyn Newton <newton.evyn@gmail.com>
2021-01-06 14:21:49 -0500

    fixed getProduct route

--- client/src/screens/Detail/Detail.jsx
+++ client/src/screens/Detail/Detail.jsx
@@ -25,34 +25,39 @@ const Detail = (props) => {
     fetchProduct();
   }, [id]);
 
-  if (!isLoaded) {
-    return <h1>The journey is worth the wait!</h1>;
-  }
   return (
     <Layout user={props.user}>
-      <div className="product-detail">
-        <div className="name">{product.name}</div>
-        <div className="price">{`${product.price}`}</div>
-        <div className="description">{product.description}</div>
-        <img className="detail-image" src={product.imgURL} alt={product.name} />
-        <Link className="edit-link" to={`/edit-product/${product._id}`}>
-          <button className="edit-button">
-            <FontAwesomeIcon icon={faPencilAlt} />
+      {isLoaded ? (
+        <div className="product-detail">
+          <div className="name">{product.name}</div>
+          <div className="price">{`${product.price}`}</div>
+          <div className="description">{product.description}</div>
+          <img
+            className="detail-image"
+            src={product.imgURL}
+            alt={product.name}
+          />
+          <Link className="edit-link" to={`/edit-product/${product._id}`}>
+            <button className="edit-button">
+              <FontAwesomeIcon icon={faPencilAlt} />
+            </button>
+          </Link>
+          <button
+            className="delete-button"
+            onClick={() => deleteProduct(product._id)}
+          >
+            <FontAwesomeIcon icon={faTrashAlt} />
           </button>
-        </Link>
-        <button
-          className="delete-button"
-          onClick={() => deleteProduct(product._id)}
-        >
-          <FontAwesomeIcon icon={faTrashAlt} />
-        </button>
-        <button
-          className="cart-button"
-          onClick={() => shopProduct(product._id)}
-        >
-          <FontAwesomeIcon icon={faCartPlus} />
-        </button>
-      </div>
+          <button
+            className="cart-button"
+            onClick={() => shopProduct(product._id)}
+          >
+            <FontAwesomeIcon icon={faCartPlus} />
+          </button>
+        </div>
+      ) : (
+        <h1>The journey is worth the wait!</h1>
+      )}
     </Layout>
   );
 };
