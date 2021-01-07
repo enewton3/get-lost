406637b96367a1352cbaddf95f2ba38456f5b508
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-01-07 15:15:10 -0500

    footer mostly done

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,13 +1,21 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare, faInstagramSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div>
+      <div className="our-name">
         <FontAwesomeIcon icon={faCopyright} /> 2021 The Digital Nomads
       </div>
+      <div className="social-icons">
+        <FontAwesomeIcon className="facebook" icon={faFacebookSquare} />
+        <FontAwesomeIcon className="insta" icon={faInstagramSquare} />
+        <FontAwesomeIcon className="twit" icon={faTwitterSquare} />
+        <FontAwesomeIcon className="email-footer" icon={faEnvelope} />
+      </div>
+
     </div>
   );
 }
