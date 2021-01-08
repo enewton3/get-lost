03d158a413463cd01bad0c8baa63e1fdea05d177
Evyn Newton <newton.evyn@gmail.com>
2021-01-08 10:49:16 -0500

    dynamically changing classnames in carousel

--- client/src/components/Carousel/ArrowLeft.jsx
+++ client/src/components/Carousel/ArrowLeft.jsx
@@ -3,8 +3,13 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function ArrowLeft(props) {
   return (
-    <div onClick={props.onClick} className="carousel-arrow-left">
-      <FontAwesomeIcon  className= "lefty" icon={faArrowLeft} />
+    <div
+      onClick={props.onClick}
+      className={
+        props.type ? `carousel-arrow-left-${props.type}` : "carousel-arrow-left"
+      }
+    >
+      <FontAwesomeIcon className="lefty" icon={faArrowLeft} />
     </div>
   );
 }
