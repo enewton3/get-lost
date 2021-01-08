c55e6696de2990ac23259f051c7517b8d25f276c
Evyn Newton <newton.evyn@gmail.com>
2021-01-08 09:47:47 -0500

    getting on the same page

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <input className="search-bar" type="text" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
     </>
   );
 }
