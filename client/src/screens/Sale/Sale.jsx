782ae3467cf0acc45dd409f8f29d6724c7e81cf7
Shelby /sexton <shelbysexton@Shelbys-MacBook-Air.local>
2021-01-09 15:41:21 -0500

    sale page created and mostly done

---
+++ client/src/screens/Sale/Sale.jsx
@@ -0,0 +1,17 @@
+import "./Sale.css";
+import Layout from "../../components/shared/Layout/Layout";
+
+
+export default function Sale() {
+  
+  return (
+  
+   <Layout>
+    <div className="sale">
+        <h1 className="sale-title">SALESALESALESALESALESALE</h1>
+        <button className= "sale-butt">Enter Here For Our Biggest Sales Event. -EVER-</button>
+        <h1 className= "sale-title2">SALESALESALESALESALESALE</h1>
+      </div>
+      </Layout>
+  );
+}
