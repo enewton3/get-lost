7c53d196f9a090f7b67b398b1f61e18571cdc553
Evyn Newton <newton.evyn@gmail.com>
2021-01-09 17:54:57 -0500

    clearing a warning for deploy

--- client/src/screens/Sale/Sale.jsx
+++ client/src/screens/Sale/Sale.jsx
@@ -1,19 +1,22 @@
 import "./Sale.css";
 import Layout from "../../components/shared/Layout/Layout";
 
-
 export default function Sale() {
-  
   return (
-  
-   <Layout>
-    <div className="sale">
+    <Layout>
+      <div className="sale">
         <h1 className="sale-title">SALESALESALESALESALESALE</h1>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
-        <button className="sale-butt">Enter Here For Our Biggest Sales Event. -EVER-</button>
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="sale-butt">
+            Enter Here For Our Biggest Sales Event. -EVER-
+          </button>
         </a>
-        <h1 className= "sale-title2">SALESALESALESALESALESALE</h1>
+        <h1 className="sale-title2">SALESALESALESALESALESALE</h1>
       </div>
-      </Layout>
+    </Layout>
   );
 }
